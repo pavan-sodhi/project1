a90e2a8f4cb0f11f6749584d0df471af811dc0df
Pavandeep Singh <pavan.sodhi@gmail.com>
2017-10-20 20:38:57 -0400

    hr rule

--- htmltags.php
+++ htmltags.php
@@ -2,6 +2,10 @@
 class htmltags
 {
 
+    static public function horizontalRule()
+    {
+        return '<br><hr>';
+    }
     static public function headingOne($text)
     {
         return '<h1><b><i>' . $text . '</i></b></h1>';
